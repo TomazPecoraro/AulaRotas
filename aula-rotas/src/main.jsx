b0801dd01f037ef6b9862f95543e44dea3e68228
tomazpecoraro <tomazpecoraro0@gmail.com>
2023-09-14 18:11:14 -0300

    Método CreateBrowserRouter

--- aula-rotas/src/main.jsx
+++ aula-rotas/src/main.jsx
@@ -6,10 +6,32 @@ import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Home from './assets/Home/index.jsx'
 import Produtos from './assets/Produtos/index.jsx'
 import Error from './assets/Error/index.jsx'
-import EditarProdutos from './assets/EdiarProdutos/index.jsx'
+import EditarProdutos from './assets/EditarProdutos/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+const router = createBrowserRouter([
+{
+  path:"/",
+  element: <App/>,
+  errorElement: <Error/>,
+  children:[
+    {
+      path: "/",
+      element: <Home/>
+    },
+    {
+      path: "/produtos",
+      element: <Produtos/>
+    },
+    {
+      path: "/produtos/editar/:id",
+      element: <EditarProdutos/>
+    }
+  ]
+}
+])
