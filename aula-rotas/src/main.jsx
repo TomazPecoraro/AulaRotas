48f0146e137ae323b4deaa6ced4881cacdcdb2ea
tomazpecoraro <tomazpecoraro0@gmail.com>
2023-09-14 18:01:05 -0300

    Importantos Routers para a página Main

--- aula-rotas/src/main.jsx
+++ aula-rotas/src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Home from './assets/Home/index.jsx'
+import Produtos from './assets/Produtos/index.jsx'
+import Error from './assets/Error/index.jsx'
+import EditarProdutos from './assets/EdiarProdutos/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
