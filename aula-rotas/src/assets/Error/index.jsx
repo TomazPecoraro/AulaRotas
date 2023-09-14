d02f1d7d9b8a54d7114755ce21ede5cdd91a8295
tomazpecoraro <tomazpecoraro0@gmail.com>
2023-09-14 17:55:01 -0300

    Função Error

--- aula-rotas/src/assets/Error/index.jsx
+++ aula-rotas/src/assets/Error/index.jsx
@@ -2,7 +2,7 @@ export default function Error(){
 
     return(
         <main>
-            <h1>Error 04 - Página não encontrada</h1>
+            <h1>Error 04 - Página não encontradaa</h1>
         </main>
     )
 }
